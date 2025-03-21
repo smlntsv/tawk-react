51367832d2da830070f14fc6da11f7d8045f6e3f
Dima <smolentsev_dn@icloud.com>
2025-03-21 19:53:34 +0100

    feat: export hooks and TawkEvent from main

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,4 @@
 export { TawkLiveChat } from './components/taw-live-chat'
+export { useTawkEvent } from './hooks/use-tawk-event'
+export { useTawkAction } from './hooks/use-tawk-action'
+export { TawkEvent } from './types/tawk'
